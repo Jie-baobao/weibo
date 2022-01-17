e4335322b6d607b3f88af00c1d1bb911b5d7cc70
jiebaobao <965814584@qq.com>
2022-01-17 22:48:47 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,14 +19,23 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
-    //处理用户注册信息
     public function store(Request $request)
     {
+        //处理用户注册信息
         $this->validate($request, [
-            'name' => 'required|unique:users|min:3|max:50',
+            'name' => 'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        //保存注册信息并重定向至个人主页
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
