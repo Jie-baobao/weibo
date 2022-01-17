afe9e72a2bd64155646de01f7c9a8c0b509597fb
jiebaobao <965814584@qq.com>
2022-01-17 21:44:22 +0800

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'StaticPagesController@home')->name('home');
-Route::get('/help', 'StaticPagesController@help')->name('help');
-Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('/', 'StaticPagesController@home')->name('home');        //主页
+Route::get('/help', 'StaticPagesController@help')->name('help');        //帮助页
+Route::get('/about', 'StaticPagesController@about')->name('about');     //关于页
 
 Route::get('signup', 'UsersController@create')->name('signup');     //用户注册
+Route::resource('users', 'UsersController');
