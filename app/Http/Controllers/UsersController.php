a412e965510aab0a050683632ca9031b0a6cc639
jiebaobao <965814584@qq.com>
2022-01-17 22:19:33 +0800

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,13 +7,26 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    //访问返回用户注册页面
     public function create()
     {
         return view('users.create');
     }
 
+    //用户个人信息页面
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    //处理用户注册信息
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|min:3|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
