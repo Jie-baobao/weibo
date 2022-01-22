803638573d035c1a0c53f7676d3974b6ec63c4a2
vagrant <vagrant@homestead>
2022-01-22 04:38:43 +0000

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -29,14 +29,20 @@ class SessionsController extends Controller
 
        //登录成功访问上一请求或用户页
        if (Auth::attempt($credentials, $request->has('remember'))) {
-        session()->flash('success', '欢迎回来！');
-        $fallback = route('users.show', Auth::user());
-        return redirect()->intended($fallback);
-    } else {
-        //登录失败返回登录页
-        session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-        return redirect()->back()->withInput();
-    }
+        if(Auth::user()->activated) {
+           session()->flash('success', '欢迎回来！');
+           $fallback = route('users.show', Auth::user());
+           return redirect()->intended($fallback);
+       } else {
+           Auth::logout();
+           session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');     //检查激活状态并反馈用户
+           return redirect('/');
+       }
+       } else {
+            //登录失败返回登录页
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+       }
     }
 
     public function destroy()
